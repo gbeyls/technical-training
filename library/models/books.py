57c90ca177b2904ee0287caa4259e7409466b768
gbeyls <40241571+gbeyls@users.noreply.github.com>
2018-10-02 08:20:23 +0000

    First afternoon

--- library/models/books.py
+++ library/models/books.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import api, models, fields
 
 class Books(models.Model):
     _name = 'library.book'
@@ -11,3 +11,25 @@ class Books(models.Model):
     isbn = fields.Char(string='ISBN')
     publisher_id = fields.Many2one('library.publisher', string='Publisher')
     rental_ids = fields.One2many('library.rental', 'book_id', string='Rentals')
+    status = fields.Selection([
+    ('borrowed', 'Borrowed'),
+    ('available', 'Available')
+    ], string='Status',compute='_compute_status',store='True',readonly='True')
+    on_late = fields.Boolean(string='Late',compute='_compute_late',store='True',readonly='True')
+        
+    
+    @api.depends('rental_ids') 
+    def _compute_status(self):
+        self.status='available'
+        if self.rental_ids :
+            last_rental=self.rental_ids.sorted(key=lambda r: r.return_date)[0]
+            if not last_rental.returned :
+                self.status='borrowed'
+                
+    @api.depends('rental_ids') 
+    def _compute_late(self):
+        self.on_late=False
+        if self.rental_ids :
+            last_rental=self.rental_ids.sorted(key=lambda r: r.return_date)[0]
+            if fields.Date.today() > last_rental.return_date :
+                self.on_late=True
--- library/models/rentals.py
+++ library/models/rentals.py
@@ -1,11 +1,45 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, exceptions, _
+from odoo import models, fields, api, _
+from openerp.exceptions import ValidationError 
 
 class Rentals(models.Model):
     _name = 'library.rental'
     _description = 'Book rental'
 
-    customer_id = fields.Many2one('library.partner', string='Customer')
-    book_id = fields.Many2one('library.book', string='Book')
+    customer_id = fields.Many2one('library.partner', string='Customer',#default=
+                                 )
+    customer_address = fields.Text(string='Address', readonly=True)
+    customer_email = fields.Char(string='Email', readonly=True)
+    book_id = fields.Many2one('library.book', string='Book',#default='_compute_book_default_value'
+                             )
+    book_authors_ids = fields.Many2many(string="Authors", related='book_id.authors_ids', readonly=True)
+    book_edition_date =  fields.Date(string='Edition date', related='book_id.edition_date', readonly=True)
+    book_isbn = fields.Char(string='ISBN',related='book_id.isbn', readonly=True)
+    book_publisher_id = fields.Many2one(string='Publisher',related='book_id.publisher_id', readonly=True)
     rental_date = fields.Date(string='Rental date')
     return_date = fields.Date(string='Return date')
+    
+    returned = fields.Boolean(string="Returned")
+    
+    @api.constrains('rental_date','return_date')
+    def _check_timeline(self):
+        if self.return_date < self.rental_date :
+            raise ValidationError("Start date must be before end date.")
+            
+    @api.constrains('book_id')
+    def _check_returned(self):
+            if self.book_id.status :
+                raise ValidationError("Book is not yet returned. Be patient.")
+            
+    def _compute_customer_default_value(self):
+
+        return
+    
+    def _compute_book_default_value(self):
+
+        return
+        
+    @api.onchange('customer_id')
+    def onchange_customer_id(self):
+            self.customer_address = self.customer_id.address
+            self.customer_email = self.customer_id.email
